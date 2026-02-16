9943a20cf76c6866f8d3c7a0cefb7968408e1f9d
oguzhantugrulakcay <oguzhantugrulakcay@hotmail.com>
2026-02-16 22:36:05 +0300

    some fixes

--- app/projects/article.tsx
+++ app/projects/article.tsx
@@ -1,12 +1,11 @@
-import type { Project } from "@/.contentlayer/generated";
+import type { Project } from "contentlayer/generated";
 import Link from "next/link";
 
 type Props = {
 	project: Project;
-	views: number;
 };
 
-export const Article: React.FC<Props> = ({ project, views }) => {
+export const Article: React.FC<Props> = ({ project }) => {
 	return (
 		<Link href={`/projects/${project.slug}`}>
 			<article className="p-4 md:p-8">
@@ -14,20 +13,16 @@ export const Article: React.FC<Props> = ({ project, views }) => {
 					<span className="text-xs duration-1000 text-zinc-200 group-hover:text-white group-hover:border-zinc-200 drop-shadow-orange">
 						{project.date ? (
 							<time dateTime={new Date(project.date).toISOString()}>
-								{Intl.DateTimeFormat(undefined, { dateStyle: "medium" }).format(
+								{new Intl.DateTimeFormat("tr-TR", { dateStyle: "medium" }).format(
 									new Date(project.date),
 								)}
 							</time>
 						) : (
-							<span>SOON</span>
+							<span>Yakında</span>
 						)}
 					</span>
-					<span className="text-zinc-500 text-xs  flex items-center gap-1">
-						{" "}
-						{Intl.NumberFormat("en-US", { notation: "compact" }).format(views)}
-					</span>
 				</div>
-				<h2 className="z-20 text-xl font-medium duration-1000 lg:text-3xl text-zinc-200 group-hover:text-white font-display">
+				<h2 className="z-20 text-xl font-medium duration-1000 lg:text-3xl text-zinc-200 group-hover:text-white font-display mt-4">
 					{project.title}
 				</h2>
 				<p className="z-20 mt-4 text-sm  duration-1000 text-zinc-400 group-hover:text-zinc-200">
